390b387f952b08d892eba91d258760c2cc3edfc9
hada3009 <th30092011@gmail.com>
2025-10-05 10:19:20 +0700

    Create gm_database.js

---
+++ assets/javascript/gm_database.js
@@ -0,0 +1,43 @@
+let name_gm =  '{"name":[' +
+            '{*001. Magnus Carlsen},' +
+            '{*002. Hikaru Nakamura},' +
+            '{*003. Arjun Erigaisi},' +
+            '{*004. Bobby Fischer},' +
+            '{*005. Mikhail Tal},' +
+            '{*006. Boris Spassky}]}';
+let name_special =  '{"special":[' +
+            '{*001. Magnus Carlsen},' +
+            '{*002. Hikaru Nakamura},' +
+            '{*003. Arjun Erigaisi},' +
+            '{*004. Bobby Fischer},' +
+            '{*005. Mikhail Tal},' +
+            '{*006. Boris Spassky}]}';
+let info_gm_1 =  '{"info":[' +
+            '{Sven Magnus Øen Carlsen (born 30 November 1990) is a Norwegian chess grandmaster. Carlsen is a five-time World Chess Champion, five-time World Rapid Chess Champion, and the reigning eight-time World Blitz Chess Champion. He has held the No.1 position in the FIDE world chess rankings since 1 July 2011 and trails only Garry Kasparov in time spent as the highest-rated player in the world. His peak rating of 2882 is the highest in history. He also holds the record for the longest unbeaten streak at the elite level in classical chess at 125 games.},' +
+            '{Christopher Hikaru Nakamura (born December 9, 1987) is an American chess grandmaster, streamer, YouTuber, five-time U.S. Chess Champion, and the reigning World Fischer Random Chess Champion. A chess prodigy, he earned his grandmaster title at the age of 15, the youngest American at the time to do so. With a peak rating of 2816, Nakamura is the tenth-highest-rated player in history.},' +
+            '{Arjun Kumar Erigaisi (born 3 September 2003) is an Indian chess grandmaster. A chess prodigy, he earned the title of grandmaster at the age of 14 years, 11 months, 13 days. In September 2024 he became India top rated player, and in December 2024 he achieved his peak rating of 2801 which makes him the fifteenth-highest rated player in history and second Indian ever to cross the 2800 threshold after Viswanathan Anand. He has sometimes been described as a madman over the board for his bold and unpredictable style.},' +
+            '{*004. Bobby Fischer},' +
+            '{*005. Mikhail Tal},' +
+            '{*006. Boris Spassky}]}';
+let info_gm_2 =  '{"info":[' +
+            '{A chess prodigy, Carlsen finished first in the C group of the Corus chess tournament shortly after he turned 13 and earned the title of grandmaster a few months later. At 15, he won the Norwegian Chess Championship, and later became the youngest ever player to qualify for the Candidates Tournament in 2005. At 17, he finished joint first in the top group of Corus. He surpassed a rating of 2800 at 18, the youngest at the time to do so. In 2010, at 19, he reached No.1 in the FIDE world rankings, the youngest person ever to do so.},' +
+            '{Nakamura has represented the United States at seven Chess Olympiads (2006, 2008, 2010, 2012, 2014, 2016, 2018), securing a team gold medal and two team bronze medals, and participated in three Candidates Tournaments, finishing second in 2024 edition, fourth in 2022 edition, and seventh in the 2016 edition. In May 2014, when FIDE began publishing official rapid and blitz chess ratings, Nakamura ranked No. 1 in the world on both lists; he has remained at or near the No. 1 rank in rapid and blitz ever since.},' +
+            '{*003. Arjun Erigaisi},' +
+            '{*004. Bobby Fischer},' +
+            '{*005. Mikhail Tal},' +
+            '{*006. Boris Spassky}]}';
+let info_gm_3 =  '{"info":[' +
+            '{Carlsen became World Chess Champion in 2013 by defeating Viswanathan Anand. He retained his title against Anand the following year and won both the 2014 World Rapid Championship and World Blitz Championship, becoming the first player to hold all three titles simultaneously, a feat which he repeated in 2019 and 2022. He defended his classical world title against Sergey Karjakin in 2016, Fabiano Caruana in 2018, and Ian Nepomniachtchi in 2021. Carlsen declined to defend his title in 2023, citing a lack of motivation.},' +
+            '{Since 2018, Nakamura has pursued a career as a content creator and subsequently signed with an esports organization TSM, later joining Misfits Gaming and then Team Falcons. Having popular channels on Twitch, Kick and YouTube, Nakamura is the most popular chess streamer and has been credited with contributing to the growth in popularity of online chess.},' +
+            '{*003. Arjun Erigaisi},' +
+            '{*004. Bobby Fischer},' +
+            '{*005. Mikhail Tal},' +
+            '{*006. Boris Spassky}]}';
+let info_gm_4 =  '{"info":[' +
+            '{Known for his attacking style as a teenager, Carlsen has since developed into a universal player. He uses a variety of openings to make it harder for opponents to prepare against him and reduce the utility of pre-game computer analysis. In 2025, he signed with esports organization Team Liquid.},' +
+            '{*002. Hikaru Nakamura},' +
+            '{*003. Arjun Erigaisi},' +
+            '{*004. Bobby Fischer},' +
+            '{*005. Mikhail Tal},' +
+            '{*006. Boris Spassky}]}';
+const gm_name = JSON.parse(name_gm)
